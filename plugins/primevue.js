0e4e410d8bf6a77e65fe0ca4bccce810bbc43fbf
Bruno Rodrigues <brunorodripeix@gmail.com>
2024-11-15 15:10:18 -0300

    Update

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -1,8 +1,6 @@
-import { defineNuxtPlugin } from '#app';
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.css';
-import 'primeicons/primeicons.css';
+import { defineNuxtPlugin } from '#app'
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue);
